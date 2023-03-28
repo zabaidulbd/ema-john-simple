992e5de7e7bcaa320d3e75e0e3c841ecbf8f2a64
zabaidulbd <zislam5622@gmail.com>
2023-03-28 12:39:31 +0600

    add product info

--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -6,7 +6,13 @@ const Product = (props) => {
     return (
         <div className='product'>
             <img src={img} alt="" />
-            <h6>{name}</h6>
+            <div className='product-info'>
+                <h6 className='product-title'>{name}</h6>
+                <span>Price: ${price}</span><br />
+                <span>Manufacturer: {seller}</span><br />
+                <span>Rating: {ratings} stars</span><br />
+            </div>
+            <button className='btn-cart'>Add to Cart</button>
         </div>
     );
 };
