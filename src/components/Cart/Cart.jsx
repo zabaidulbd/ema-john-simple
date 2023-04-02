1537b1230f81dfc015ad199e0f00e132de4b521b
zabaidulbd <zislam5622@gmail.com>
2023-04-02 11:56:49 +0600

    add rest of cart component

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -7,10 +7,18 @@ const Cart = ({ cart }) => {
 
     let totalPrice = 0;
     let totalShipping = 0;
-
+    let quantity = 0;
     for (const product of cart) {
-        totalPrice = totalPrice + product.price;
+
+        if (product.quantity === 0) {
+            product.quantity = 1;
+        }
+
+        // product.quantity == product.quantity || 1;  uporer condition ta ke nicher vabeo lekha jay.....
+
+        totalPrice = totalPrice + product.price * product.quantity;
         totalShipping = totalShipping + product.shipping;
+        quantity = quantity + product.quantity;
     }
 
     const tax = totalPrice * 7 / 100;
@@ -19,7 +27,7 @@ const Cart = ({ cart }) => {
     return (
         <div className='order-summary'>
             <h1>Order Summary</h1>
-            <p>Selected Item: {cart.length}</p>
+            <p>Selected Item: {quantity}</p>
             <p>Total Price: ${totalPrice} </p>
             <p>Total Shipping Charge: ${totalShipping}</p>
             <p>Tax: ${tax.toFixed(2)} </p>
--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -35,7 +35,22 @@ const Shop = () => {
     }, [products])
 
     const handleCart = (product) => {
-        const newCart = [...cart, product];
+
+        let newCart = [];
+        // const newCart = [...cart, product];
+        // if product doesn't exit in the cart, then set quantity = 1
+        // if exist update quantity by 1
+        const exists = cart.find(pd => pd.id === product.id);
+        if (!exists) {
+            product.quantity = 1;
+            newCart = [...cart, product]
+        }
+        else {
+            exists.quantity = exists.quantity + 1;
+            const remaining = cart.filter(pd.id !== product.id);
+            newCart = [...remaining, exists]
+        }
+
         setCart(newCart);
         addToDb(product.id);
     }
